6d261cc024f2d54c9164e20a40b798e2fea049d7
SabaLeshkasheli <salopa0911@gmail.com>
2024-09-17 10:55:36 +0400

    third commit

---
+++ level02/classwork/cw.py
@@ -0,0 +1,23 @@
+#print ("GOA Best")
+
+
+# ცვლადი - Variable
+
+car = "მაქვინი"
+
+# str string -ბრჭყალებში მოქცეული ტექსტი
+# მიმნიჭებელი ოპერატორი - =
+
+age = 30
+number = 30.5
+
+# int - სრული ციფრი
+#float - წილადი ციფრი
+# კონკარინაცია
+
+print (car + " aris " + str(age) + " wlis")
+
+num1 = "10"
+num2 = "20"
+print(num1 + " + " + num2 + " = " + str(int(num1) + int(num2)))
+
---
+++ level02/homework/variables.py
@@ -0,0 +1,13 @@
+#მიმატება
+#გამრავლება
+#გამოკლება
+#გაყოფა
+
+num1 = "54"
+num2 = "85"
+
+
+print(num1 + " + " + num2 + " = " + str(int(num1) + int(num2)))
+print(num1 + " * " + num2 + " = " + str(int(num1) * int(num2)))
+print(num1 + " - " + num2 + " = " + str(int(num1) - int(num2)))
+print(num1 + " / " + num2 + " = " + str(int(num1) / int(num2)))

56a6cd45563d731062212a28a777c41e64babba9
SabaLeshkasheli <salopa0911@gmail.com>
2024-09-23 17:03:24 +0400

    fifth commit

---
+++ level04/classwork/cw.py
@@ -0,0 +1,17 @@
+#დავალები: მომხმარებელს შემოატანინეთ ორი რიცხვი, შემდეგ კი გაკეთეთ ამ რიცხვს შორის შეკრება
+
+num1 = int(input("put your age : "))
+
+num2 = int(input("put your pet's age : "))
+
+print(num1 + num2)
+
+#დავალება: მომხამრებლს შემოატანინეთ 2 რიცხვი, ხოლო შემდეგ გაკეთეთ ამ რიცხვებს შორის მათემატიკური ოპერაცია, და შეინახეთ ცვლადში, ხოლო ბოლოს დაპრინტეთ ცვლადის მნიშვნელობა.
+
+num1 = int(input("put a random number : "))
+
+num2 = int(input("put a random number: "))
+
+result = num1 / num2
+
+print(result , num1 , num2)
---
+++ level04/homework/hw.py
@@ -0,0 +1,42 @@
+
+#დავალება: მომხმარებელს შემოატანინეთ 2 რიცხვი, ხოლო შემდეგ გაკეთეთ ამ რიცხვებზე ყველა მათემატიკური ოპერაცია ცალცალკე.
+
+num1 = int(input("Put a random number : "  ))
+num2 = int(input("a second random nubver : "  ))
+
+result = num1 + num2
+
+print(result, num1 , num2)
+
+result = num1 * num2
+
+print(result, num1, num2)
+
+result = num1 - num2
+
+print(result, num1, num2)
+
+result = num1 / num2
+
+print(result, num1 ,num2)
+
+
+#დავალება: კომენტარიბის სახით ახსენით თქვენი სიტყვებით, რა არის input, და output
+
+#input aris roca magalitad adamiani registracias gadis da uwevs emailis/usernamis da parolis sheqmna , aseve input sheidzleba iyos mausi, klaviatura, yursasmenebi da ase shemdeg
+#output aris roca adamiani daresgistrirdeba da mas sakutari username aqvs da paroli aseve output sheidzleba iyos dinamikebi , ekrani, printeri da ase shemdeg
+
+
+
+
+
+
+
+
+
+
+#დავალება: მომხმარებელს შემოატანინეთ თავისი სახელი და გავარი, შემდეგ კი ტერმინალში გამოიტანეთ მომხმარებლის შემოტანილი მნიშვნელობები
+Name = input("Put your name here : " )
+Surname = input("Put your surname here : ")
+
+print(Name + Surname)
